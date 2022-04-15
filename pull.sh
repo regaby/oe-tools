13733edd49f638f45b207a9a83d454dd2bc9a523
Gabriela Rivero <regaby@gmail.com>
2022-04-15 17:06:49 -0300

    ADD pull sh

---
+++ pull.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+#
+# Script para hacer pull de un repositorio
+# parameters: repositorio 
+# por ejemplo ./only_pull.sh telco-modules
+
+repository=$1
+
+CD="/odoo_ar/odoo-15.0e/vangrow/sources"
+
+cd "$CD/$repository"
+echo "***** actualizando repositorio $repository ******"
+git pull
+echo "***** reiniciando servidor ******"
+python3 ../multi_client_mgr/multi.py -s -r -c canal2
