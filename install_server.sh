b10f295c2ce04c486cfa04f5d88aa7d642cf3825
Administrador <administrador@NBKTAB006365UBUNTU.ad.mirgor.com.ar>
2023-10-22 18:34:08 -0300

    [UPD] auto install

--- install_server.sh
+++ install_server.sh
@@ -2,7 +2,7 @@
 #sudo apt upgrade -y
 sudo apt install git -y
 sudo apt install python3-distutils -y
-sudo apt install python3-pip
+sudo apt install python3-pip -y
 sudo pip install odoo-env
 curl -fsSL get.docker.com -o get-docker.sh
 sh get-docker.sh
